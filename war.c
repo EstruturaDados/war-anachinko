241dd4ea13bc0966f55a6121d734e3581596ff99
anachinko <ana.0409.chinko@gmail.com>
2025-12-02 13:34:05 +0000

    commit

--- war.c
+++ war.c
@@ -96,3 +96,51 @@ int main() {
 
 // limparBufferEntrada():
 // Função utilitária para limpar o buffer de entrada do teclado (stdin), evitando problemas com leituras consecutivas de scanf e getchar.
+#include <stdio.h>
+#include <string.h>
+
+/*
+    Estrutura Territorio:
+    Armazena o nome do território, a cor do exército e a quantidade de tropas.
+*/
+typedef struct {
+    char nome[30];
+    char cor[10];
+    int tropas;
+} Territorio;
+
+int main() {
+    // Para armazenar os 5 territórios
+    Territorio territorios[5];
+
+    printf("=== Cadastro de Territorios ===\n\n");
+
+    // Entrada dos dados:
+    for (int i = 0; i < 5; i++) {
+        printf("Cadastro do territorio %d:\n", i + 1);
+
+        printf("Digite o nome do territorio: ");
+        scanf("%s", territorios[i].nome);
+
+        printf("Digite a cor do exercito: ");
+        scanf("%s", territorios[i].cor);
+
+        printf("Digite a quantidade de tropas: ");
+        scanf("%d", &territorios[i].tropas);
+
+        printf("\n");
+    }
+
+    // Exibir os dados depois do cadastro
+    printf("\n=== Territorios Cadastrados ===\n\n");
+
+    for (int i = 0; i < 5; i++) {
+        printf("Territorio %d:\n", i + 1);
+        printf("Nome: %s\n", territorios[i].nome);
+        printf("Cor do exercito: %s\n", territorios[i].cor);
+        printf("Tropas: %d\n", territorios[i].tropas);
+        printf("---------------------------\n");
+    }
+
+    return 0;
+}
